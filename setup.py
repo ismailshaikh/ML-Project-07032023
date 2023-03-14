ed71831ab0c4d47d15dd5176ae043a525f902351
Mohammed Ismail <ismail46h.shaikh@gmail.com>
2023-03-15 05:21:02 +0530

    logging and exception created and Working Amazing

--- setup.py
+++ setup.py
@@ -14,10 +14,26 @@ def get_requirements(file_path:str)->List[str]:
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
         
     
     return requirements
 
+# def get_requirements(file_path:str)->List[str]:
+#     '''
+#     This function will return the list of requirements
+#     '''
+#     HYPEN_e_DOT = '-e .'
+#     requirements=[]
+    
+#     with open(file_path) as file_obj:
+#         requirements = file_obj.readlines()
+#         requirements = [req.replace("\n","") for req in requirements]
+
+#         if HYPEN_e_DOT is in requirements:
+#             requirements.remove(HYPEN_e_DOT)
+            
+#     return requirements
 
 
 # Metadata information of Entire project
