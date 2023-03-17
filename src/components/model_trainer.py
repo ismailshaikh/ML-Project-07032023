5c78e2ef88091055fcb2da771d7ea64bb2124ec5
Mohammed Ismail <ismail46h.shaikh@gmail.com>
2023-03-18 02:07:29 +0530

    Model Trainer code started

---
+++ src/components/model_trainer.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object,evaluate_models
+
+
+@ dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    
+
+class ModelTrainer:
+    def __init__(self) :
+        self.model_trainer_config = ModelTrainerConfig()
+        
+        
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array=[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]            
+            )
+            
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regression": KNeighborsRegressor(),
+                "CatBoosting Regression": CatBoostRegressor(verbose=False)
+                "AdaBoost Regression": AdaBoostRegressor()  
+            
+            }
+            
+            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train,x_test=X_test,y_test=y_test,
+                                                models=models)
+            
+            ## TO get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+            
+            ## To get Best modeel name from dict
+            best_model_name = list(model_report.values())[
+                list(model_report.values()).index(best_model_dcore)
+            ]
+            
+        except Exception as e:
+            raise CustomException(e,sys)
