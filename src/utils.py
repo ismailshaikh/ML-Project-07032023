6e82809851e59d96ec3e96d21c8cfe07de8891e7
Mohammed Ismail <ismail46h.shaikh@gmail.com>
2023-03-19 04:00:31 +0530

    Hyperparameter added

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import dill
 
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
@@ -22,7 +23,7 @@ def save_object(file_path,obj):
 
 
 
-def evaluate_model(X_train, y_train, X_test,y_test,models):
+def evaluate_model(X_train, y_train, X_test,y_test,models,param):
     try:
         # X_train,X_test,y_train,y_test = train_test_split(
         #     X, y, test_size=0.2, random_state=42        
@@ -30,11 +31,18 @@ def evaluate_model(X_train, y_train, X_test,y_test,models):
         report = {}
         
         for i in range(len(list(models))):
-            
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
             # Model Training
+            # model.fit(X_train,y_train)
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
